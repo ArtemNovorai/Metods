632c45d2fd974ae20ee0952185f841ddce636c9d
ArtemNovorai <artnovorai@gmail.com>
2017-07-02 01:03:10 +0300

    Added new metods

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ do {
   var correct = confirm("Ваш логин - " + login + "\nПодтверди данные");
 }
 while(!correct);
-alert("Ваш логин - " + login);      
+alert("Ваш логин - " + login); 
+
+
+var a, b;
+a = 100;
+b = 555;
+alert(a+=b);
+
+eval("alert(124454 + 44431213)");
+eval("alert('Artem ' + ' Novorai')")
