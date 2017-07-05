802c164bf6f8d03269f801c345d7e3e362bdc941
ArtemNovorai <artnovorai@gmail.com>
2017-07-05 21:45:16 +0300

    Added new materials: Array

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 do {
   var login = prompt("Введите Ваш логин: ");
   var correct = confirm("Ваш логин - " + login + "\nПодтверди данные");
@@ -13,3 +14,12 @@ alert(a+=b);
 
 eval("alert(124454 + 44431213)");
 eval("alert('Artem ' + ' Novorai')")
+*/
+
+var w = [1,'hello',67,'werber',5,345];
+ for(var index in w) {
+document.write('Ячейка массива - ' + index + ' - значение данной ячейки - ' + w[index] + '<br>' );
+   
+   var arr = [1,2,3,4,['Artem','Novorai']];
+   document.write(arr[4][1] + '<br>');
+ }
